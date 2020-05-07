9b71c0706b3bd5d766ee0d03c31fe3bc1628d8d1
Myronov Denys <myronovdenys@gmail.com>
2020-05-07 20:05:22 +0300

    add group divided just by gender

--- course/week2/UnvariateAnalisPractice.py
+++ course/week2/UnvariateAnalisPractice.py
@@ -12,8 +12,22 @@ da["marital_status"] = da.DMDMARTL.replace({1: "Unmarried", 2: "Married", 3: "Wi
 da["gender"] = da.RIAGENDR.replace({1: 'Male', 2: 'Female'})
 da["agegrp"] = pd.cut(da.RIDAGEYR, [18, 30, 40, 50, 60, 70, 80])
 
-da_male = da.loc[(da.gender == 'Male') & da.agegrp.isin([30, 40])]
-da_female = da.loc[(da.gender == 'Female') & da.agegrp.isin([30, 40])]
+da_male_all = da.loc[(da.gender == 'Male')]
+da_female_all = da.loc[(da.gender == 'Female')]
+da_male_30_40 = da.loc[(da.gender == 'Male') & da.agegrp.isin([30, 40])]
+da_female_30_40 = da.loc[(da.gender == 'Female') & da.agegrp.isin([30, 40])]
+
+print("All:")
 print(da.marital_status.value_counts())
-print(da_male.marital_status.value_counts())
-print(da_female.marital_status.value_counts())
+
+print("\n All Males")
+print(da_male_all.marital_status.value_counts())
+
+print("\n All Females")
+print(da_female_all.marital_status.value_counts())
+
+print("\n Males 30-40 year:")
+print(da_male_30_40.marital_status.value_counts())
+
+print("\n Females 30-40 year:")
+print(da_female_30_40.marital_status.value_counts())
